82ab86272330aaadcb7f8801ff06a34edbe0dcfb
d4vves <d4vves@gmail.com>
2021-02-19 12:46:27 -0600

    wow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Nick is Cool')
+    res.send('Dave is Cool')
 })
 
-app.listen(3000, () => console.log('Nick is cool'))
+app.listen(3000, () => console.log('Dave is cool'))
